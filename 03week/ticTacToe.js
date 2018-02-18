ee148149cee4ed5133ea2ce9346f09078dfa6919
Andrew <32229265+aselee@users.noreply.github.com>
2018-02-17 19:21:16 -0600

    class5 - ticTacToe draft with Win functions

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -34,29 +34,52 @@ const playerTwo = 'O';
 
 function horizontalWin() {
   // Your code here
-  //if(board[0][0] === playerTurn && board[1][0] === playerTurn && board[0][2] === playerTurn)}
-  //return true;
-  //else if{board[0][1]}
-  if(board[0][0])
-
-
+  if(board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] ===
+  playerTurn) {
+    return true;
+  } else if (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2]
+  === playerTurn) {
+    return true;
+  } else if (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2]
+  === playerTurn) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function verticalWin() {
   // Your code here
-  const vertiWinArr1 = ['0','3','6']
-  const vertiWinArr2 = ['1','4','7']
-  const vertiWinArr3 = ['2','5','8']
+  if(board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] ===
+  playerTurn) {
+    return true;
+  } else if(board[0][1] === playerTurn && board[1][1] === playerTurn && board[1][2]
+  === playerTurn) {
+    return true;
+  } else if(board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2]
+  === playerTurn) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function diagonalWin() {
   // Your code here
-  const diaWinArr1 = ['0','4','8']
-  const diaWinArr2 = ['2','4','6']
+  if(board[0][0] === playerTurn && board[1][1] === playerTurn && board[2][2] ===
+  playerTurn) {
+    return true;
+  } else if(board[0][2] === playerTurn && board[1][1] === playerTurn && board[2][0]
+  === playerTurn) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function checkForWin() {
   // Your code here
+
 }
 
 function ticTacToe(row, column) {
