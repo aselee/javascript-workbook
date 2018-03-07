8632e1a79e042d5cc4e2c33612f9442e6c630fd3
Andrew <32229265+aselee@users.noreply.github.com>
2018-03-06 22:07:48 -0600

    initial

---
+++ 07week/controlledinput/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			inputValue: ''
+		};
+	}
+
+	handleInputChange = e => {
+		this.setState({ inputValue: e.target.value });
+	};
+	render() {
+		return (
+			<div className="App">
+				<input onChange={this.handleInputChange} type="text" />
+				<button>Submit</button>
+			</div>
+		);
+	}
+}
+
+export default App;
